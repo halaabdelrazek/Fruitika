ec5e68453bbfe144a03035935a2c57acbf6859ea
saifodin <saifodeen.w@gmail.com>
2022-04-12 21:34:50 +0200

    create catergory repository

---
+++ ecommerceProjectMVC/Repositories/CategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using ecommerceProjectMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ecommerceProjectMVC.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        ContextEntities context;
+
+        public CategoryRepository(ContextEntities _context)
+        {
+            context = _context;
+        }
+
+        public List<Category> GetAll()
+        {
+            return context.Categories.ToList();
+        }
+
+        public Category GetByID(int id)
+        {
+            return context.Categories.FirstOrDefault(c => c.CategoryId == id);
+        }
+
+        public int Insert(Category category)
+        {
+            context.Categories.Add(category);
+
+            int raw = context.SaveChanges();
+            return raw;
+        }
+
+        public int Update(int id, Category category)
+        {
+            Category oldCatergory = GetByID(id);
+            if (oldCatergory != null)
+            {
+                oldCatergory.Name = category.Name;
+                oldCatergory.Description = category.Description;
+                return context.SaveChanges();
+            }
+            return 0;
+        }
+
+        public int Delete(int id)
+        {
+            context.Categories.Remove(GetByID(id));
+            return context.SaveChanges();
+        }
+
+    }
+}
---
+++ ecommerceProjectMVC/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using ecommerceProjectMVC.Models;
+using System.Collections.Generic;
+
+namespace ecommerceProjectMVC.Repositories
+{
+    public interface ICategoryRepository
+    {
+        int Delete(int id);
+        List<Category> GetAll();
+        Category GetByID(int id);
+        int Insert(Category category);
+        int Update(int id, Category category);
+    }
+}
